33bdda48dac5a5555addf20b87a5b70d3f0a0ba3
Huy Nguyễn <81611311+HuyHaHa00@users.noreply.github.com>
2026-06-10 15:57:08 +0700

    feat: initialize Firebase services and authentication with persistence for web platform

--- src/app/config/firebase.web.ts
+++ src/app/config/firebase.web.ts
@@ -1,5 +1,6 @@
 // src/app/config/firebase.web.ts
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import {
   Auth,
   browserLocalPersistence,
@@ -25,6 +26,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6Le6EhctAAAAAJTIhUyoUw2JkrOctEAC18eh7JDp"),
+  isTokenAutoRefreshEnabled: true,
+});
+
 // Trên Web: Khởi tạo trực tiếp không dùng bất kỳ thư viện mobile nào
 const auth: Auth = initializeAuth(app, {
   persistence: [browserLocalPersistence, browserSessionPersistence],
@@ -39,3 +45,4 @@ const db = initializeFirestore(app, {
 const storage = getStorage(app);
 
 export { app, auth, db, storage };
+
